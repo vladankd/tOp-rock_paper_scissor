47493f00e5e6f0ddff6b91a1b92e9737d12ff42d
Vladan Babic <vladankd@gmail.com>
2023-05-05 22:41:40 +0200

    Add basic styles

--- Scripts/script.js
+++ Scripts/script.js
@@ -40,27 +40,34 @@ function computerOption() {
 function decideWinner(player, computer) {
   let playerOption = player.toLowerCase();
   let computerOption = computer.toLowerCase();
+  let playerWon;
+  let tie = false;
 
   if (playerOption === computerOption) {
     console.log(`Player has choosen: ${playerOption}`);
     console.log(`Computer has choosen: ${computerOption}`);
     console.log("tie");
+    return (tie = true);
   } else if (playerOption === "rock" && computerOption === "scissor") {
     console.log(`Player has choosen: ${playerOption}`);
     console.log(`Computer has choosen: ${computerOption}`);
     console.log(`${playerOption} beats ${computerOption}, player won`);
+    return (playerWon = true);
   } else if (playerOption === "paper" && computerOption === "rock") {
     console.log(`Player has choosen: ${playerOption}`);
     console.log(`Computer has choosen: ${computerOption}`);
     console.log(`${playerOption} beats ${computerOption}, player won`);
+    return (playerWon = true);
   } else if (playerOption === "scissor" && computerOption === "paper") {
     console.log(`Player has choosen: ${playerOption}`);
     console.log(`Computer has choosen: ${computerOption}`);
     console.log(`${playerOption} beats ${computerOption}, player won`);
+    return (playerWon = true);
   } else {
     console.log(`Player has choosen: ${playerOption}`);
     console.log(`Computer has choosen: ${computerOption}`);
     console.log(`${playerOption} loses to ${computerOption}, computer won`);
+    return (playerWon = false);
   }
 }
 
